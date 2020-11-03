9af1b30f6a0646baa8f0cab7d6918c02bfc57c94
Alex Shuliar <shoulyar@live.com>
2020-11-03 06:59:01 +0200

    Create prestentation Level

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public SearchController(IBookRepository bookRepository) {
+            this.bookRepository = bookRepository;
+        }
+        public IActionResult Index(string quary)
+        {
+            var books = bookRepository.GetAllByTitle(quary);
+            return View(books);
+        }
+    }
+}
