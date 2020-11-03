d11a19149053f9e86ba8d4b96e20af2ad06b68df
Alex Shuliar <shoulyar@live.com>
2020-11-03 06:32:13 +0200

    Create class Book

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -4,5 +4,13 @@ namespace Store
 {
     public class Book
     {
+        public int Id;
+        public string Title { get;  }
+
+        public Book(int id,string title) {
+            Id = id;
+            Title = title;
+        }
+
     }
 }
