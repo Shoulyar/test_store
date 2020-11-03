6a800433d33f850f9e856c63a02105922fd2f092
Alex Shuliar <shoulyar@live.com>
2020-11-03 06:47:02 +0200

    Create infrastucture and Create Store memory project

---
+++ infrastructure/StoreMemory/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using Store;
+using System;
+using System.Linq;
+
+namespace StoreMemory
+{
+    public class BookRepository : IBookRepository
+
+    {
+        private readonly Book[] books = new[]
+        {
+        new Book(1, "Art Of Programming"),
+        new Book(2, "All About Scram"),
+        new Book(1, "C Programming language")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
