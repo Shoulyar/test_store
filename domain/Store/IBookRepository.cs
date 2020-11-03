00658b0a0b735aba0ede8ae27c4e9f278027dded
Alex Shuliar <shoulyar@live.com>
2020-11-03 06:36:31 +0200

    Create IBookRepository

---
+++ domain/Store/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    interface IBookRepository
+    {
+        Book[] GetAllByTitle(string titlePart);
+    }
+}
