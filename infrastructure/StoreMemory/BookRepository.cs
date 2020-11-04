68acfc8c7809e1d5176b2802c635e8f2c12b14b9
Alex Shuliar <shoulyar@live.com>
2020-11-04 07:59:50 +0200

    Create search by Id

--- infrastructure/StoreMemory/BookRepository.cs
+++ infrastructure/StoreMemory/BookRepository.cs
@@ -11,8 +11,13 @@ namespace StoreMemory
         {
         new Book(1, "Art Of Programming", "ISBN 12345-54321","D. Knut", "Big book1", 7.19M),
         new Book(2, "All About Scram", "ISBN 11111-00000","M. Flauer", "Big book2", 79.19M),
-        new Book(1, "C Programming language", "ISBN 12345-54322","L. Kerning", "Big book3", 57.19M)
+        new Book(3, "C Programming language", "ISBN 12345-54322","L. Kerning", "Big book3", 57.19M)
         };
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
+        }
 
         public Book[] GetAllByIsbn(string isbn)
         {
